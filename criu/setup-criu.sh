e84c94d0defb7765f2534ae577de3c517fc94f03
John Goen <jtgoen@gmail.com>
2018-02-21 13:07:06 -0700

    Small changes to setup. Hacky, but will cleanup later

--- criu/setup-criu.sh
+++ criu/setup-criu.sh
@@ -5,11 +5,12 @@ go get -u github.com/golang/protobuf/protoc-gen-go
 cd go/src/github.com && mkdir checkpoint-restore && cd checkpoint-restore
 git clone -b criu-dev https://github.com/checkpoint-restore/criu.git
 #git clone https://github.com/checkpoint-restore/criu.git
+cd criu #&& git revert --no-commit 62629e4 # checkout tags/v3.6
 
 sudo chown -R ubuntu /home/ubuntu/go
 
 # Build/Check CRIU
-cd criu/ && sudo make install
+sudo make install
 criu check
 
 # Test CRIU
--- criu/setup-pkg.sh
+++ criu/setup-pkg.sh
@@ -2,6 +2,6 @@
 
 apt-get -y -q update
 apt-get -y -q install \
-    unzip git-all gcc syslinux-utils \
+    unzip git-all gcc syslinux-utils gdb \
     libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler protobuf-compiler python-protobuf \
     pkg-config python-ipaddr iproute2 libcap-dev libnl-3-dev libnet-dev libaio-dev python-yaml asciidoc xmlto --no-install-recommends
